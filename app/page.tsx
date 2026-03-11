b281427ff3a24467270899769f15f1ddfe6003bb
hugchen <hugchen@hugchendeMacBook-Air.local>
2026-03-12 00:32:13 +0800

    merge github repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
-import { defaultLocale } from "@/lib/i18n";
 
 export default function RootPage() {
-  redirect(`/${defaultLocale}`);
+  redirect("/en");
 }
