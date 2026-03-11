1b584f53a765ffc4705557ce384d15dff5327c9c
hugchen <hugchen@hugchendeMacBook-Air.local>
2026-03-12 00:40:25 +0800

    fix root redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Page() {
+  const headersList = await headers();
+  const acceptLanguage = headersList.get("accept-language") || "";
+
+  if (acceptLanguage.startsWith("zh")) {
+    redirect("/zh");
+  }
+
   redirect("/en");
 }
